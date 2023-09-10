7b2e5e1d8fc8a74477cc38d3ae886dde1a3bb878
Iwumathie <iwumatthew@gmail.com>
2023-09-10 13:28:48 -0700

    seting up time

--- task1.js
+++ task1.js
@@ -1,8 +1,4 @@
-const now = new Date();
 
-const year = now.getUTCFullYear();
-const month = now.getUTCMonth() + 1;
-const day = now.getUTCDate();
 const hours = now.getUTCHours();
 const minutes = now.getUTCMinutes();
 const seconds = now.getUTCSeconds();
